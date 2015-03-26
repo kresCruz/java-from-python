1dff4bf2d1588ca4160eda06a9242522fcd96918
kresCruz <krescruz@gmail.com>
2015-03-25 18:55:20 -0600

    Argument to Send parameters at run program

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ class PyJavaRun(object):
 	_exe = 'java'
 	_options = {}
 	
-	def __init__(self, name=None):
+	def __init__(self, name=None, params=[]):
 		self._name = name
 		self._options = ['-jar']
+		self._params = params
 		self.run()
 
 	def command_args(self):
 		options = ''.join(self._options)
-		command = [self._exe, options, self._name]
+		command = [self._exe, options, self._name] + self._params
 		return command
 
 	def run(self, stdout=PIPE, stderr=STDOUT):
