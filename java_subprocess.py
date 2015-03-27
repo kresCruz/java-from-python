c76808cbdb21c9ffb02e1f2f92fdabc74de49d5f
kresCruz <krescruz@gmail.com>
2015-03-27 17:03:28 -0600

    Add 'timeout' parameter

--- java_subprocess.py
+++ java_subprocess.py
@@ -1,13 +1,13 @@
-from subprocess import Popen, STDOUT, PIPE
+from subprocess import Popen, STDOUT, PIPE, TimeoutExpired
 
 
 class PyJavaRun(object):
 	"""
 	PyJavaRun is a class that run a java program through subprocess class
 	"""
-	_exe = 'java'
+	_exec = 'java'
 	
-	def __init__(self, name=None, params=[]):
+	def __init__(self, name=None, timeout=None, params=[]):
 		if not type(name) == str:
 			raise TypeError(
 				'The `name` must be a str. Got %s.' %
@@ -23,20 +23,30 @@ class PyJavaRun(object):
 		self._name = name
 		self._options = ['-jar']
 		self._params = params
+		self._timeout = timeout
 		self.run()
 
 	def command_args(self):
 		options = ''.join(self._options)
-		command = [self._exe, options, self._name] + self._params
+		command = [self._exec, options, self._name] + self._params
 		return command
 
 	def run(self, stdout=PIPE, stderr=STDOUT):
+		"""
+		Run command through subprocess.communicate() method.
+		"""
 		args = self.command_args()
 		process = Popen(args, stdout=stdout, stderr=stderr)
-		self._output, self._errors = process.communicate()
-		self._returncode = process.returncode
-		process.kill()
 
+		try:
+			self._output, self._errors = process.communicate(timeout=self._timeout)
+			self._returncode = process.returncode
+			process.kill()
+		except TimeoutExpired:
+			raise
+		finally:
+			process.kill()
+		
 	@property
 	def returncode(self):
 		return self._returncode
